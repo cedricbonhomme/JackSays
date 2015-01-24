e8a5dfbb2f9529dd929f27598c6df865c3baf77f
Christophe Goessen <chris@fleming>
2015-01-24 02:04:15 +0100

    Game template

--- web/models.py
+++ web/models.py
@@ -12,3 +12,17 @@ class User(object):
     def dump(self):
         return {"nic": self.nic,
                 "score": self.score}
+
+class Game(object):
+    """
+    Reprensent a mini game
+    """
+    def __init__(self):
+        self.user_vals = {}
+    def user_input(self, username, param):
+        self.user[username] = param
+    def finalize(self):
+        """
+        overload this method
+        """
+        pass
