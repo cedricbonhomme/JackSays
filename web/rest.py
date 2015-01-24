b7cf9fa6b01b1d569cc9af534d0fb08719a74c27
Cédric Bonhomme <cedric@cedricbonhomme.org>
2015-01-24 13:27:01 +0100

    ...

---
+++ web/rest.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import g, Response, request, session, jsonify
+from flask.ext.restful import Resource, reqparse
+
+from web import api
+
+
+class ScoreAPI(Resource):
+    """
+    Defines a RESTful API for Article elements.
+    """
+    #method_decorators = [authenticate]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        super(ScoreAPI, self).__init__()
+
+    def get(self):
+        """
+        Returns a list of articles.
+        """
+        return jsonify(result= {
+                                    "score": 10,
+                                    "nic": "John",
+                                    "avatar": "m123"
+                                }
+                        )
+api.add_resource(ScoreAPI, '/api/v1/scores', endpoint = 'scores.json')
