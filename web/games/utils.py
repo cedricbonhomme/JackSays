eeb89045d2d1c7bc1352e2fb677a819346c714b6
Cédric Bonhomme <cedric@cedricbonhomme.org>
2015-01-24 15:34:11 +0100

    Bugfix.

--- web/games/utils.py
+++ web/games/utils.py
@@ -5,6 +5,7 @@ import time
 
 class WaitGame (Game):
     def __init__(self):
+        super(self.__class__, self).__init__()
         self.game_id = "wait"
         self.start_time= time.time()
         self.duration = 60
