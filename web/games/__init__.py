dce3a21ed321e7a66cce384ebd86770bc2e0d55b
Christophe Goessen <chris@fleming>
2015-01-25 13:19:00 +0100

    add gif

--- web/games/__init__.py
+++ web/games/__init__.py
@@ -1,7 +1,7 @@
 from web.models import Game
 
 from game1 import Game1
-from utils import WaitGame, PickOne, Roma, Shake, Scream, Click
+from utils import WaitGame, PickOne, Roma, Shake, Scream, Click, Still
 
 
-game_list = [PickOne(), Roma(), Shake(), Scream(), Click()]
+game_list = [Still(),Still(),Still(),Still(),Still(), Roma(), Shake(), Scream(), Click()]
