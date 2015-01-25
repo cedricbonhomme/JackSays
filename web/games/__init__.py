d7361c5a996523554e0230fa2b01b629fc7ba230
Cédric Bonhomme <cedric@cedricbonhomme.org>
2015-01-25 01:47:27 +0100

    New games.

--- web/games/__init__.py
+++ web/games/__init__.py
@@ -1,8 +1,7 @@
 from web.models import Game
 
 from game1 import Game1
-from utils import WaitGame
-from utils import PickOne
+from utils import WaitGame, PickOne, Roma
 
 
-game_list = [PickOne()]
+game_list = [Roma()]
--- web/games/utils.py
+++ web/games/utils.py
@@ -1,11 +1,15 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+import random
 from web.models import Game
 import time
 
 
 
 class WaitGame (Game):
+    """
+    Used for waiting before the first game starts.
+    """
     def __init__(self):
         super(self.__class__, self).__init__()
         self.game_id = "wait"
@@ -19,10 +23,53 @@ class WaitGame (Game):
     def finalize(self):
         pass
 
+#
+# Games definition
+#
+ 
 class PickOne (Game):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.game_id = "pickone"
+        self.start_time= time.time()
+        self.duration = 10
+        self.data = {}
+
+    def get_data(self):
+        return self.duration
+
+    def user_input(self, username, data):
+        if username not in self.data:
+            self.data[username] = time.time()
+
+    def finalize(self):
+        if self.data.items() != []:
+            result = reduce(lambda x,y: x if self.data[x]<=self.data[y] else y, self.data.iterkeys())
+
+class Roma (Game):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.game_id = "roma"
+        self.start_time= time.time()
+        self.duration = 10
+        self.data = {}
+
+    def get_data(self):
+        self.number = random.randint(4, 9)
+        return self.number
+
+    def user_input(self, username, data):
+        if username not in self.data and data["button"]==self.number:
+            self.data[username] = time.time()
+
+    def finalize(self):
+        if self.data.items() != []:
+            result = reduce(lambda x,y: x if self.data[x]<=self.data[y] else y, self.data.iterkeys())
+
+class Sound (Game):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.game_id = "sound"
         self.start_time= time.time()
         self.duration = 10
         self.data = {}
